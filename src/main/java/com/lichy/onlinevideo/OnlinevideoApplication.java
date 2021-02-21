48752e000d2489fc6c9c6a06ab74a5f00bb6a261
lichy <lichyseter@hotmail.com>
2021-02-22 00:35:40 +0800

    Initial commit

---
+++ src/main/java/com/lichy/onlinevideo/OnlinevideoApplication.java
@@ -0,0 +1,13 @@
+package com.lichy.onlinevideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinevideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlinevideoApplication.class, args);
+    }
+
+}
