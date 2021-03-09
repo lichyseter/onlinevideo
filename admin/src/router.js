cf9997c1683a8e477be0834b65258720c2510d9b
lichy <lichyseter@hotmail.com>
2021-03-09 23:46:15 +0800

    提交admin页面

---
+++ admin/src/router.js
@@ -0,0 +1,22 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Login from "./views/login.vue"
+import Admin from "./views/admin.vue"
+
+Vue.use(Router);
+
+export default new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [{
+        path: "*",
+        redirect: "/login",
+    }, {
+        path: "/login",
+        component: Login
+    }, {
+        path: "/admin",
+        name: "admin",
+        component: Admin
+    }]
+})
--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -48,7 +48,9 @@
                             <span class="lbl"> 记住我</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button type="button"
+                                  v-on:click="login()"
+                                  class="width-35 pull-right btn btn-sm btn-primary">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登录</span>
                           </button>
@@ -74,7 +76,12 @@
 <script>
 
 export default {
-  name: 'App'
+  name: 'login',
+  methods:{
+    login(){
+      this.$router.push("admin");
+    }
+  }
 }
 </script>
 
