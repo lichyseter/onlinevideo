065bdc3ebc96809efcccdd874ddb278435124586
lichy <lichyseter@hotmail.com>
2021-03-27 16:13:47 +0800

    提交bussiness代码

--- business/src/main/java/com/lichy/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/lichy/business/controller/admin/ChapterController.java
@@ -1,29 +1,61 @@
 package com.lichy.business.controller.admin;
 
-import com.lichy.server.domain.Chapter;
+import com.lichy.server.dto.ChapterDto;
+import com.lichy.server.dto.ChapterPageDto;
+import com.lichy.server.dto.ResponseDto;
 import com.lichy.server.service.ChapterService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lichy.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin/chapter")
 public class ChapterController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
+    public static final String BUSINESS_NAME = "大章";
 
     @Resource
     private ChapterService chapterService;
 
     /**
-     * search list
+     * 列表查询
      */
-    @GetMapping("/list")
-    public List<Chapter> list() {
-        return chapterService.listByCourse("");
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody ChapterPageDto chapterPageDto) {
+        ResponseDto responseDto = new ResponseDto();
+        ValidatorUtil.require(chapterPageDto.getCourseId(), "课程ID");
+        chapterService.list(chapterPageDto);
+        responseDto.setContent(chapterPageDto);
+        return responseDto;
     }
 
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    @PostMapping("/save")
+    public ResponseDto save(@RequestBody ChapterDto chapterDto) {
+        // 保存校验
+        ValidatorUtil.require(chapterDto.getName(), "名称");
+        ValidatorUtil.require(chapterDto.getCourseId(), "课程ID");
+        ValidatorUtil.length(chapterDto.getCourseId(), "课程ID", 1, 8);
+
+        ResponseDto responseDto = new ResponseDto();
+        chapterService.save(chapterDto);
+        responseDto.setContent(chapterDto);
+        return responseDto;
+    }
+
+    /**
+     * 删除
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseDto delete(@PathVariable String id) {
+        ResponseDto responseDto = new ResponseDto();
+        chapterService.delete(id);
+        return responseDto;
+    }
 }
---
+++ business/src/main/java/com/lichy/business/controller/web/CategoryController.java
@@ -0,0 +1,35 @@
+package com.lichy.business.controller.web;
+
+import com.lichy.server.dto.CategoryDto;
+import com.lichy.server.dto.ResponseDto;
+import com.lichy.server.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController("webCategoryController")
+@RequestMapping("/web/category")
+public class CategoryController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryController.class);
+    public static final String BUSINESS_NAME = "分类";
+
+    @Resource
+    private CategoryService categoryService;
+
+    /**
+     * 列表查询
+     */
+    @PostMapping("/all")
+    public ResponseDto all() {
+        ResponseDto responseDto = new ResponseDto();
+        List<CategoryDto> categoryDtoList = categoryService.all();
+        responseDto.setContent(categoryDtoList);
+        return responseDto;
+    }
+}
